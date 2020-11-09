509541e2c42de059eebec175af17bd41c1130f78
GitHub282207 <2676890377@qq.com>
2020-11-09 09:11:41 +0800

    购物车

--- zhuanzhuan/pages/shopcar/shopcar.js
+++ zhuanzhuan/pages/shopcar/shopcar.js
@@ -5,14 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    address:{},
+    cart:[]
+  },
+  //获取收货地址wx.chooseAddress
+  handleAddress(){
+    //获取用户权限状态
+    wx.getSetting({
+      success: (result) => {
+       const scopeAddress = result.authSetting["scope.address"];
+       if(scopeAddress===true||scopeAddress===undefined){
+         wx.chooseAddress({
+           success: (address) => {
+            //  console.log(result1);
+            wx.setStorageSync("address", address);
+           }          
+         });
+             }else{
+         wx.openSetting({
+           success: (res) => {
+            wx.chooseAddress({
+              success: (result3) => {
+                // console.log(result3);
+            wx.setStorageSync("address", address);
+           }
+              
+            });
+           }
+         });
+           
+       }
+      }
+    });
+    
+      
+      
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+        
   },
 
   /**
@@ -26,7 +60,16 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+     
+    //获取缓存中收货地址信息
+    const address = wx.getStorageSync("address");
+    const cart = wx.getStorageSync("cart");
+      // console.log(address);
+    this.setData({
+      address,
+      cart
+    })
+      
   },
 
   /**
